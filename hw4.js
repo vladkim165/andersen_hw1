98cc2f640bee965931313d40798346d4c205efec
vladkim165@gmail.com <vladkim165@gmail.com>
2021-12-15 16:52:37 +0300

    added/hw4

---
+++ hw4.js
@@ -0,0 +1,8 @@
+const createDebounceFunction = (func, wait) => {
+  let timeout;
+  return function (...args) {
+    const context = this;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(context, args), wait);
+  };
+}
