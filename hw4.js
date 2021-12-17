fe2cf11b0b26b739a51ec03a6914d8b063b53c5e
Vladislav <vladkim165@gmail.com>
2021-12-17 17:20:52 +0300

    Update hw4.js

--- hw4.js
+++ hw4.js
@@ -5,10 +5,3 @@ const createDebounceFunction = (func, wait) => {
     clearTimeout(timeout);
     timeout = setTimeout(() => func.apply(context, args), wait);
   };
-<<<<<<< HEAD
-};
-
-module.exports = createDebounceFunction;
-=======
-}
->>>>>>> 98cc2f640bee965931313d40798346d4c205efec
